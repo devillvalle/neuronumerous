fa44d032bb6fd943a3709a208472887821b018cf
Christian Gruber <christianedwardgruber@gmail.com>
2010-06-21 16:28:18 -0400

    Hook up UI to getting data from files, and pull out old Main class infrastructure.

--- src/main/java/com/neuronumerous/defcon/lies/Main.java
+++ src/main/java/com/neuronumerous/defcon/lies/Main.java
@@ -4,14 +4,6 @@
  */
 package com.neuronumerous.defcon.lies;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
-import java.util.logging.Logger;
-
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
@@ -20,62 +12,15 @@ import org.javabuilders.BuilderConfig;
 import org.javabuilders.swing.SwingJavaBuilder;
 
 import com.google.inject.Injector;
-import com.neuronumerous.defcon.lies.data.PolyData;
 import com.neuronumerous.defcon.lies.data.PolyDataImpl;
-import com.neuronumerous.defcon.lies.data.PolyDataSource;
-import com.neuronumerous.defcon.lies.data.PolyDataUtil;
 import com.neuronumerous.defcon.lies.data.PolyGraphModel;
 import com.neuronumerous.defcon.lies.ui.MainFrame;
 import com.neuronumerous.defcon.lies.ui.PolyGraph;
 import com.neuronumerous.defcon.lies.util.ConfigUtil;
-import com.neuronumerous.defcon.lies.util.Source;
-import com.neuronumerous.defcon.lies.util.Ticker;
 
 public class Main {
   
-  public static String APP_PACKAGE = "com/neuronumerous/defcon/lies";
-  public static Injector APP_INJECTOR;
-  private final Logger logger;
-  private final PolyDataUtil polyDataUtil;  
-  
-  public Main(Logger logger, PolyDataUtil polyDataUtil) {
-    this.logger = logger;
-    this.polyDataUtil = polyDataUtil;
-  }
-
-
-
-  
-  
-  
-  /**
-   * Main event loop.
-   */
-  public void run() throws InterruptedException {
-    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    //BufferedReader reader = Files.newReader(aFile, Charsets.US_ASCII);
-    runFromReader(reader);
-  }
-
-  public void runFromReader(BufferedReader reader) throws InterruptedException {
-    Source<PolyData> source = new PolyDataSource(reader, logger);
-    PolyData pd = source.next();
-    Ticker ticker = new Ticker();
-		while (pd != null) {
-		  // Aggregate
-      PolyData currentPd = pd;
-		  List<PolyData> pds = new ArrayList<PolyData>();
-		  while (currentPd != null && currentPd.getTimestamp() == pd.getTimestamp()) {
-		    pds.add(currentPd);
-		    currentPd = source.next();
-		  }
-		  pd = polyDataUtil.aggregatePolyDatas(pd.getTimestamp(), pds);
-		  System.out.println(pd);
-		  pd = currentPd;
-		  ticker.block(1, TimeUnit.SECONDS);
-		}
-	}
-
+//  public static String APP_PACKAGE = "com/neuronumerous/defcon/lies";
 
   private static void registerCustomComponents(BuilderConfig config) {
     config.addType("PolyGraph", PolyGraph.class);
@@ -85,16 +30,13 @@ public class Main {
    * Main entry point
    */
   public static void main(String[] args) throws InterruptedException {
-//    APP_INJECTOR = Guice.createInjector(new MainModule());
-//    Main app = APP_INJECTOR.getInstance(Main.class);
-//    Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
-//      @Override public void run() {
-//        System.out.println("Shutdown detected - exiting.");
-//        System.exit(0);
-//      }
-//    }));
-//    app.run();
-    
+  
+    Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+      @Override public void run() {
+        System.out.println("Shutdown detected - exiting.");
+        System.exit(0);
+      }
+    }));
 
     SwingUtilities.invokeLater(new Runnable() {
       public void run() {
--- src/main/java/com/neuronumerous/defcon/lies/ui/MainFrame.java
+++ src/main/java/com/neuronumerous/defcon/lies/ui/MainFrame.java
@@ -1,8 +1,17 @@
 package com.neuronumerous.defcon.lies.ui;
 
+import java.awt.event.ActionEvent;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Random;
+import java.util.logging.Logger;
 
+import javax.swing.JDialog;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 import org.javabuilders.BuildResult;
 import org.javabuilders.annotations.DoInBackground;
@@ -10,18 +19,31 @@ import org.javabuilders.event.BackgroundEvent;
 import org.javabuilders.event.CancelStatus;
 import org.javabuilders.swing.SwingJavaBuilder;
 
+import com.neuronumerous.defcon.lies.data.PolyData;
 import com.neuronumerous.defcon.lies.data.PolyDataImpl;
+import com.neuronumerous.defcon.lies.data.PolyDataSource;
 import com.neuronumerous.defcon.lies.data.PolyGraphModel;
+import com.neuronumerous.defcon.lies.util.Source;
 
 @SuppressWarnings("unused")
 public class MainFrame extends JFrame {
   private static final long serialVersionUID = 6493344221859264556L;
+  private static final Logger logger = Logger.getLogger(MainFrame.class.getName());
 
   private BuildResult       result;
 
   private PolyGraphModel    polyModel;
   
   private BackgroundEvent   currentStartEvent = null;
+  
+  private Source<PolyData>  source;
+  
+  private File              currentFolder;
+
+  private File              input;
+  
+  private File              output;
+  
 
   public PolyGraphModel getPolyModel() {
     return polyModel;
@@ -34,40 +56,137 @@ public class MainFrame extends JFrame {
   public MainFrame() {
     result = SwingJavaBuilder.build(this);
   }
+  
+  private void onFileMenuSimulate() {
+    this.source = new Source<PolyData>() {
+      private final Random r = new Random();      
+      public PolyData next() {
+        try{ 
+          Thread.sleep(100); 
+        } catch (InterruptedException e) {
+          return null;
+        }
+        return new PolyDataImpl(0, 95 + r.nextInt(10), 140 + r.nextInt(20), 130 + r.nextInt(10), 120 + r.nextInt(10));
+      }
+    };
+  } 
+  
+  private void onFileMenuStream() {
+    logger.info("onFileMenuStream was invoked!"); 
+    File input = selectFile("Open input device or file to be streamed...");
+    if (input != null && input.exists()) {
+      this.input = input;
+    } else {
+      this.input = null;
+      this.output = null;
+      return;
+    }
+    File output = selectFile("Open file to save the streamed data...");
+    if (output != null && output.exists()) {
+      this.output = output;
+    } else {
+      this.output = null;
+      return;
+    }
+    try {
+      BufferedReader reader = new BufferedReader(new FileReader(input));
+      this.source = new PolyDataSource(reader, Logger.getLogger(PolyDataSource.class.getName())){
+        @Override public PolyData next() {
+          PolyData pd = super.next();
+          logger.info("Processing PolyData node: " + pd);
+          return pd;
+        }
+      };
+    } catch (FileNotFoundException e) {
+      logger.severe("File not found: " + input);
+      this.source = null;
+      this.input = null;
+      this.output = null;
+    }
+     
+  } 
 
+  private void onFileMenuReplay(ActionEvent e) {
+    logger.info("onFileMenuReplay was invoked!"); 
+    File input = selectFile("Open file to be replayed...");
+    output = null;
+    if (input != null && input.exists()) {
+      this.input = input;
+    } else {
+      this.input = null;
+      return;
+    }
+    try {
+      BufferedReader reader = new BufferedReader(new FileReader(input));
+      this.source = new PolyDataSource(reader, Logger.getLogger(PolyDataSource.class.getName()));
+    } catch (FileNotFoundException fnfe) {
+      logger.severe("File not found: " + input);
+      this.source = null;
+      this.input = null;
+      this.output = null;
+    }
+  }
+  
+  private boolean isStartEnabled() {
+    return source != null;
+  }
+
+  private File selectFile(String dialogTitle) {
+    
+    JFileChooser jfc = new JFileChooser(currentFolder);
+    jfc.setFileHidingEnabled(false);
+    jfc.setDialogTitle(dialogTitle);
+
+    int returnVal = jfc.showOpenDialog(this);
+
+    if (returnVal == JFileChooser.APPROVE_OPTION) {
+      File file = jfc.getSelectedFile();
+      //This is where a real application would open the file.
+      logger.info("Opening: " + file.getName() + ".");
+      if (file != null) {
+        currentFolder = file.getParentFile();
+      }
+      return file;
+    } else {
+      logger.info("Open command cancelled by user.");
+      return null;
+    }
+  }
   @DoInBackground(cancelable = true, blocking = false)
   private void start(BackgroundEvent evt) {
-    System.out.println("Fuzzing data.");
+    logger.info("Starting visualization.");
+    if (source == null) {
+      JOptionPane.showMessageDialog(this, 
+              "Please select a data source from the menu.", 
+              "No Data Source...", 
+              JOptionPane.ERROR_MESSAGE);
+      return;
+    }
     this.currentStartEvent = evt;
-    while (true) {
-      if (evt.getCancelStatus() != CancelStatus.REQUESTED) {
-        try {
-          Thread.sleep(100);
-          fuzz();
-        } catch (InterruptedException e) {
-          break;
-        }
-      } else {
+    while (evt.getCancelStatus() != CancelStatus.REQUESTED && source != null) {
+      try {
+        populateData();
+      } catch (Throwable e) {
         break;
       }
     }
     evt.setCancelStatus(CancelStatus.PROCESSING);
-    System.out.println("Cancelling...");
+    logger.info("Cancelling visualization.");
     evt.setCancelStatus(CancelStatus.COMPLETED);
     currentStartEvent = null;
-    System.out.println("Stopped.");
+    logger.info("Visualization Stopped.");
   }
 
   private void stop() {
-    if (currentStartEvent != null) {
+    logger.info("Stopping visualization.");
+    if (currentStartEvent != null && currentStartEvent.isCancelable()) {
       currentStartEvent.setCancelStatus(CancelStatus.REQUESTED);
     }
   }
 
-  private void fuzz() {
-    Random r = new Random();
-    polyModel.offer(new PolyDataImpl(0, 95 + r.nextInt(10), 140 + r.nextInt(20), 130 + r
-            .nextInt(10), 120 + r.nextInt(10)));
+  private void populateData() {
+    PolyData pd = source.next();
+    polyModel.offer(pd);
   }
 
   private void replot() {
