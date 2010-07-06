bea9a5fd7b4590d0a7c2d1feac1dad5666347d86
Christian Gruber <christianedwardgruber@gmail.com>
2010-07-06 17:06:27 -0400

    more refactoring - just moving things into more natural places.

--- src/test/java/com/neuronumerous/defcon/lies/data/PolyDataParserTest.java
+++ src/test/java/com/neuronumerous/defcon/lies/data/PolyDataParserTest.java
@@ -8,11 +8,13 @@ import junit.framework.Assert;
 import org.junit.Test;
 
 public class PolyDataParserTest {
+  
+  PolyDataParser parser = new PolyDataParser();
 
   @Test
   public void testGetDataFieldFromLine() {
     String fieldData = "Timestamp:     0:5:4";
-    DataField result = new PolyDataParser().getDataFieldFromLine(fieldData);
+    DataField result = parser.getDataFieldFromLine(fieldData);
     Assert.assertEquals("Timestamp", result.name);
     Assert.assertEquals("0:5:4", result.data);
   }
@@ -20,8 +22,16 @@ public class PolyDataParserTest {
   @Test
   public void testGetNextNonEmptyLine() {
     BufferedReader reader = new BufferedReader(new StringReader("\n\n\nblah\n\n"));
-    Assert.assertEquals("blah", new PolyDataParser().nextNonEmptyLine(reader));
-    Assert.assertEquals(null, new PolyDataParser().nextNonEmptyLine(reader));
+    Assert.assertEquals("blah", parser.nextNonEmptyLine(reader));
+    Assert.assertEquals(null, parser.nextNonEmptyLine(reader));
   }
 
+  @Test
+  public void testParseTimestamp() {
+    Assert.assertEquals((Integer)304, parser.parseTimestamp("0:5:4"));
+    Assert.assertEquals((Integer)124, parser.parseTimestamp("0:2:4"));
+    Assert.assertEquals((Integer)124, parser.parseTimestamp(":2:4"));
+    Assert.assertEquals((Integer)4, parser.parseTimestamp("0::4"));
+    Assert.assertEquals((Integer)(65*60+4), parser.parseTimestamp("0:65:4"));
+  }
 }
