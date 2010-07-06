f978297b36c5866d283bf24035d19d6ba9485589
Christian Gruber <christianedwardgruber@gmail.com>
2010-07-06 16:18:29 -0400

    commented out but added some testing based on rxtx.org.  no source dep in this change - will re-add import statements when I uncomment.

--- src/test/java/com/neuronumerous/defcon/lies/MainIntegrationTest.java
+++ src/test/java/com/neuronumerous/defcon/lies/MainIntegrationTest.java
@@ -1,12 +1,34 @@
 package com.neuronumerous.defcon.lies;
 
-import org.junit.Test;
 
 public class MainIntegrationTest {
 
-  @Test
-  public void testParseTimestamp() throws Throwable {
-    
-  }
-
+//  @Test
+//  public void testCreate() throws Throwable {
+//    Enumeration<CommPortIdentifier> ports = CommPortIdentifier.getPortIdentifiers();
+//    while (ports.hasMoreElements()) {
+//      CommPortIdentifier port = ports.nextElement();
+//      System.out.println(port.getName() + ": " + getPortTypeName(port.getPortType()));
+//    }
+//    
+//  }
+//
+//  static String getPortTypeName ( int portType )
+//  {
+//      switch ( portType )
+//      {
+//          case CommPortIdentifier.PORT_I2C:
+//              return "I2C";
+//          case CommPortIdentifier.PORT_PARALLEL:
+//              return "Parallel";
+//          case CommPortIdentifier.PORT_RAW:
+//              return "Raw";
+//          case CommPortIdentifier.PORT_RS485:
+//              return "RS485";
+//          case CommPortIdentifier.PORT_SERIAL:
+//              return "Serial";
+//          default:
+//              return "unknown type";
+//      }
+//  }
 }
